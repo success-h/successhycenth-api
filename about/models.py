28974c49c7606d870ecc89a91be30d54e5433fdd
success hycenth <successhycenth8@@gmail.com>
2022-03-31 11:55:19 +0100

    updating branch

--- about/models.py
+++ about/models.py
@@ -1,11 +1,12 @@
 from email.policy import default
 from django.db import models
+from .validators import validate_file_extension
 
 
 class About(models.Model):
     title = models.CharField(max_length=50)
     description = models.CharField(max_length=250)
-    icon = models.FileField(upload_to='about/', null=True, blank=True, default=None)
+    icon = models.FileField(upload_to='about/', null=True, blank=True, default=None, validators=[validate_file_extension])
 
     class Meta:
         verbose_name = 'About'
--- technologies/models.py
+++ technologies/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Technology(models.Model):    
     name = models.CharField(max_length=100)
     icon = models.ImageField(upload_to='icons', null=True, blank=True, max_length=255)
+    url = models.URLField(max_length=500, null=True, blank=True)
 
     class Meta:
         verbose_name = 'Technology'
